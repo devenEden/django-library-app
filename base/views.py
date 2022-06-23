0a8bd8b2ccc67f5decab741752cddf2c0a1bf968
feta_deven <deshdeven@gmail.com>
2022-06-23 16:18:36 +0300

    added view and url for books

--- base/views.py
+++ base/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .forms import BookForm
+from .models import Book
+
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'dashboard.html', {'name': 'name'})
+    total_books = Book.objects.count();
+    books = Book.objects.all()
+    context = {'books': books, 'total_books': total_books}
+    return render(request, 'dashboard.html', context)
+
+def createBook(request):
+    form = BookForm
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'books/books_form.html')
