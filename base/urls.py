791cd92ccbdd6225a3985cdf30c6ebf485736c97
Feta Deven <devenfeta19@gmail.com>
2022-07-18 06:40:51 +0000

    Revert "Merge branch 'roles' into 'main'"
    This reverts merge request !12

--- base/urls.py
+++ base/urls.py
@@ -3,10 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('books/edit-book/<str:pk>/', views.editBook, name='edit_book'),
-    path('books/delete-book/<str:pk>/', views.deleteBook, name='delete_book'),
     path('books/create-book', views.createBook, name='create_book'),
     path('logout/', views.logoutUser, name='logout'),
     path('login/', views.loginPage, name='login'),
-    path("signup/", views.signup, name="signup"),
+    path("signup/", views.signup, name = "signup"),
 ]
