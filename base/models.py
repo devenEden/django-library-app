898876af804a08a786f6110599fa4d035dc9f063
feta_deven <deshdeven@gmail.com>
2022-07-18 09:37:40 +0300

    added roles model

--- base/models.py
+++ base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -12,3 +13,14 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Role(models.Model):
+    role = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateField(auto_now=True)
+    updated = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.role
